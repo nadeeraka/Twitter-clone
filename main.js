5c27ea916a32dba9266169c982b84642190a90d8
nadeeraka <nadeerakan@gmail.com>
2019-08-25 22:51:49 +0530

    img added

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 
 const form = document.querySelector('form');
+const loding = document.querySelector('.loding');
+
+loding.style.display='none'
 
 form.addEventListener('submit',(event)=>
 {
+    
     console.log('hi')
     event.preventDefault();
     const formData = new FormData(form);
     const name = formData.get('name')
     const data = formData.get('data')
+    if(name&&data)
+    {
+        form.style.display='none';
+        loding.style.display=''
+    }
+    else{
+        alert('plase submit the form')
+    }
 
     const dataObj = {
         name,
         data
     }
     console.log(dataObj);
+
 })
